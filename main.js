fd33597dc773c4e4df2532625a0185593601ec28
Honnok <110690701+Honnok@users.noreply.github.com>
2024-09-03 04:21:47 +0200

    V: 4.0.0
    First

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: path.join(__dirname, 'assets','autres', 'icons.ico'),
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile('index.html');
+}
+
+Menu.setApplicationMenu(null);
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
